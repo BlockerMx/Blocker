4f6ad888f8c0e56c60f7deece6fab11aabbd08ea
BlockerMx <140426983+BlockerMx@users.noreply.github.com>
2023-07-24 11:03:13 -0600

    Delete Blocker directory

--- Blocker/Inicio de sesion y registro/inicio de sesion/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("btnRegistrar").addEventListener("click", function() {
-    mostrarAlerta();
-  });
-  
-  function mostrarAlerta() {
-    alert("Lo sentimos, el servidor de base de datos actualmente se encuentra fuera de servicio.");
-  }
-  
